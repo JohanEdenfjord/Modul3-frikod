dddb1c4b75172694f80b3d54f41c052c2d9a5034
Johan Edenfjord <johan@edenfjord.se>
2020-09-21 23:27:38 +0200

    123

--- app.js
+++ app.js
@@ -21,15 +21,40 @@ let locationButton = document.querySelector("#geolocation");
 let appNameButton = document.querySelector("#appname");
 let display = document.querySelector(".display");
 const nAgt = navigator.userAgent;
+const backgroundImgButton = document.querySelector(".backgroundButton");
 
 (function () {
   function error(err) {
     display.innerHTML = `ERROR(${err.code}): ${err.message}`;
   }
 
+  backgroundLoader = () =>{
+    return new Promise((resolve, reject) => {
+    let randomNumber = Math.floor(Math.random() * 8);
+    console.log("randomnumber " + randomNumber)
+    if(randomNumber > 0 && randomNumber < 7){
+      resolve(randomNumber + ".jpg");
+    }else{
+      reject("0.jpg");
+    }
+  })}
+
+  backgroundImgButton.addEventListener("click", () =>
+    backgroundChanger()
+  );
+
+  
+    async function backgroundChanger() {
+     await backgroundLoader().then((image) => {
+      document.body.style.backgroundImage = `url(/img/${image})`
+     }).catch((image) => {
+      document.body.style.backgroundImage = `url(/wrongimg/${image})`
+     })
+    };
+    
+
   async function success(pos) {
     let crd = await pos.coords;
-
     display.innerText = `Your current position is: \n Long: ${crd.longitude} \n Lat: ${crd.latitude}\n Accuracy is about: ${crd.accuracy} meters.`;
   }
 
@@ -56,10 +81,10 @@ const nAgt = navigator.userAgent;
 
   appNameButton.addEventListener("click", () => {
     display.innerHTML = navigator.appName;
+    //heheh. something wierd huh?
   });
 
   locationButton.addEventListener("click", () => {
     navigator.geolocation.getCurrentPosition(success, error);
-    //heheh. something wierd huh?
   });
 })();
